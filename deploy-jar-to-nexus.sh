961647dafc6d96d6c2ee85537da116628beb5187
weijun <2059746@qq.com>
2025-09-29 23:22:05 +0800

    把对应的依赖都注入到私有库中了，以及留下install到私有库语句

---
+++ deploy-jar-to-nexus.sh
@@ -0,0 +1,111 @@
+#mvn deploy:deploy-file -Dfile=opensabre-framework/opensabre-web/target/opensabre-web-0.1.0.jar \
+#                       -DgroupId=io.github \
+#                       -DartifactId=opensabre-web \
+#                       -Dversion=0.1.0 \
+#                       -Dpackaging=jar \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/releases/ \
+#                       -DrepositoryId=byt
+
+#mvn deploy:deploy-file -Dfile=opensabre-framework/opensabre-base-dependencies/pom.xml \
+#                       -DgroupId=io.github \
+#                       -DartifactId=opensabre-base-dependencies \
+#                       -Dversion=0.1.0 \
+#                       -Dpackaging=pom \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/releases/ \
+#                       -DrepositoryId=byt
+
+#mvn deploy:deploy-file -Dfile=opensabre-framework/opensabre-starter-boot/target/opensabre-starter-boot-0.1.0.jar \
+#                       -DgroupId=io.github \
+#                       -DartifactId=opensabre-starter-boot \
+#                       -Dversion=0.1.0 \
+#                       -Dpackaging=jar \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/releases/ \
+#                       -DrepositoryId=byt
+
+#mvn deploy:deploy-file -Dfile=opensabre-framework/opensabre-starter-cache/target/opensabre-starter-cache-0.1.0.jar \
+#                       -DgroupId=io.github \
+#                       -DartifactId=opensabre-starter-cache \
+#                       -Dversion=0.1.0 \
+#                       -Dpackaging=jar \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/releases/ \
+#                       -DrepositoryId=byt
+
+#mvn deploy:deploy-file -Dfile=opensabre-framework/opensabre-starter-config/target/opensabre-starter-config-0.1.0.jar \
+#                       -DgroupId=io.github \
+#                       -DartifactId=opensabre-starter-config \
+#                       -Dversion=0.1.0 \
+#                       -Dpackaging=jar \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/releases/ \
+#                       -DrepositoryId=byt
+
+#mvn deploy:deploy-file -Dfile=opensabre-framework/opensabre-starter-eda/target/opensabre-starter-eda-0.1.0.jar \
+#                       -DgroupId=io.github \
+#                       -DartifactId=opensabre-starter-eda \
+#                       -Dversion=0.1.0 \
+#                       -Dpackaging=jar \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/releases/ \
+#                       -DrepositoryId=byt
+
+#mvn deploy:deploy-file -Dfile=opensabre-framework/opensabre-starter-persistence/target/opensabre-starter-persistence-0.1.0.jar \
+#                       -DgroupId=io.github \
+#                       -DartifactId=opensabre-starter-persistence \
+#                       -Dversion=0.1.0 \
+#                       -Dpackaging=jar \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/releases/ \
+#                       -DrepositoryId=byt
+
+#mvn deploy:deploy-file -Dfile=opensabre-framework/opensabre-starter-register/target/opensabre-starter-register-0.1.0.jar \
+#                       -DgroupId=io.github \
+#                       -DartifactId=opensabre-starter-register \
+#                       -Dversion=0.1.0 \
+#                       -Dpackaging=jar \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/releases/ \
+#                       -DrepositoryId=byt
+
+#mvn deploy:deploy-file -Dfile=opensabre-framework/opensabre-starter-rpc/target/opensabre-starter-rpc-0.1.0.jar \
+#                       -DgroupId=io.github \
+#                       -DartifactId=opensabre-starter-rpc \
+#                       -Dversion=0.1.0 \
+#                       -Dpackaging=jar \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/releases/ \
+#                       -DrepositoryId=byt
+
+#mvn deploy:deploy-file -Dfile=opensabre-framework/opensabre-test/target/opensabre-test-0.1.0.jar \
+#                       -DgroupId=io.github \
+#                       -DartifactId=opensabre-test \
+#                       -Dversion=0.1.0 \
+#                       -Dpackaging=jar \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/releases/ \
+#                       -DrepositoryId=byt
+
+#mvn deploy:deploy-file -Dfile=opensabre-framework/pom.xml \
+#                       -DgroupId=io.github \
+#                       -DartifactId=opensabre-framework \
+#                       -Dversion=0.1.0 \
+#                       -Dpackaging=pom \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/releases/ \
+#                       -DrepositoryId=byt
+
+#mvn deploy:deploy-file -Dfile=base-organization/target/base-organization-0.0.1-SNAPSHOT.jar \
+#                       -DgroupId=io.github \
+#                       -DartifactId=base-organization \
+#                       -Dversion=0.0.1-SNAPSHOT \
+#                       -Dpackaging=jar \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/snapshots/ \
+#                       -DrepositoryId=byt
+
+#mvn deploy:deploy-file -Dfile=base-gateway/target/base-gateway-0.0.1-SNAPSHOT.jar \
+#                       -DgroupId=io.github \
+#                       -DartifactId=base-gateway \
+#                       -Dversion=0.0.1-SNAPSHOT \
+#                       -Dpackaging=jar \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/snapshots/ \
+#                       -DrepositoryId=byt
+
+#mvn deploy:deploy-file -Dfile=base-authorization/target/base-authorization-0.0.1.jar \
+#                       -DgroupId=io.github \
+#                       -DartifactId=base-authorization \
+#                       -Dversion=0.0.1 \
+#                       -Dpackaging=jar \
+#                       -Durl=http://192.168.200.199:8081/nexus/content/repositories/releases/ \
+#                       -DrepositoryId=byt
